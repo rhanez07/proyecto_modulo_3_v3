a9038cdd51cf5c1fe62e10f22e4483bafe8468cb
ronny <rhanez07@gmail.com>
2026-05-05 00:26:33 -0400

    CATEGORIAS, RELACION CON NOTAS, ENDPOINT CON VALIDACIONES DE PRIVACIDAD

--- src/infrastructure/database/mongo/note.mongo.repository.js
+++ src/infrastructure/database/mongo/note.mongo.repository.js
@@ -8,7 +8,8 @@ export default class NoteMongoRepository {
             imageUrl: noteEntity.imageUrl,
             isPrivate: noteEntity.isPrivate,
             password: noteEntity.password,
-            userId: noteEntity.userId
+            userId: noteEntity.userId,
+            categoryId: noteEntity.categoryId,
         });
         const savedNote = await note.save();
         return savedNote.toObject();
--- src/presentation/routes/note.routes.js
+++ src/presentation/routes/note.routes.js
@@ -6,12 +6,12 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { roleMiddleware } from "../middlewares/role.middleware.js";
 
 // Importamos el repositorio de MySQL y el servicio de Mail
-import NoteMySQLRepository from "../../infrastructure/database/mysql/note.mysql.repository.js";
+import NoteMongoRepository from "../../infrastructure/database/mongo/note.mongo.repository.js";
 import MailService from "../../infrastructure/services/mail.service.js";
 
 // inyeccion de dependencias
 const mailService = new MailService();
-const noteRepository = new NoteMySQLRepository();
+const noteRepository = new NoteMongoRepository();
 const noteService = new NoteService(noteRepository, mailService);
 const noteController = new NoteController(noteService);
 
